131bc0a0d386a762e0e854c04c06009ba98e558b
a.danielswaroop <daniel.swrp@gmail.com>
2025-11-07 16:55:27 +0530

    added depencies of github pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: /simple-calculator-js/,
   plugins: [
     tailwindcss(),
   ],
